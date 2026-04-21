1c5b54cb5e889cbe43ad5dc3d1b8a91e9f30adf6
João Rocha <joao.rocha@wobcom.de>
2026-04-21 15:11:44 +0200

    feat: create_folder implemented and tested

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,57 @@
-use std::error::Error;
-use std::path::PathBuf;
+use std::{fs::create_dir, path::PathBuf};
 
 #[derive(PartialEq, Debug)]
 pub enum FolderCreationError {
     FolderAlreadyExists,
     CouldNotCreateFolder,
+    CouldNotRead,
 }
 
-pub fn create_folder(path: PathBuf) -> Result<(), FolderCreationError> {
+pub fn create_folder(path: PathBuf) -> Result<bool, FolderCreationError> {
     let exists = match path.try_exists() {
-        Ok(x) => println!("this is x: {x}"),
-        Err(x) => println!("this is errx :{x}"),
+        Ok(x) => x,
+        Err(_) => return Err(FolderCreationError::CouldNotRead),
     };
 
-    Ok(())
+    if exists {
+        return Err(FolderCreationError::FolderAlreadyExists);
+    } else {
+        match create_dir(path) {
+            Ok(()) => return Ok(true),
+            Err(_) => return Err(FolderCreationError::CouldNotCreateFolder),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempdir::TempDir;
 
     #[test]
     fn test_create_folder_success() {
-        assert_eq!(create_folder(PathBuf::from("test_folder")), Ok(()));
+        let tmp_dir = match tempdir::TempDir::new("test_folder") {
+            Ok(x) => x,
+            Err(_) => panic!("error at test_create_folder_success"),
+        };
+
+        let file_path = tmp_dir.path().join("test_folder");
+
+        assert_eq!(create_folder(PathBuf::from(file_path)), Ok(true));
     }
 
     #[test]
     fn test_create_folder_exists() {
+        let tmp_dir = match TempDir::new("test_folder_exists") {
+            Ok(x) => x,
+            Err(_) => panic!("error at test_create_folder_success"),
+        };
+
+        let file_path = tmp_dir.path().join("test_folder_exists");
+
+        let _ = create_folder(file_path.clone());
         assert_eq!(
-            create_folder(PathBuf::from("test_folder_exists")),
+            create_folder(file_path),
             Err(FolderCreationError::FolderAlreadyExists)
         )
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,1 @@
-use std::path::PathBuf;
-
-use aoc_solver::create_folder;
-
-fn main() {
-    create_folder(PathBuf::from("test_dir"));
-}
+fn main() {}
